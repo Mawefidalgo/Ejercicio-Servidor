e7b39378157ebba4054167f215aebe11a58235eb
Mawefidalgo <mawefidalgo7@gmail.com>
2023-11-06 12:44:16 +0100

    Models act, nuevo super usuario y gitignore act

--- todolist/models.py
+++ todolist/models.py
@@ -1,11 +1,7 @@
 from django.db import models
 
 # Create your models here.
-
-
-
-class Post(models.Model):
-    
+class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     completed = models.BooleanField(default=False)
