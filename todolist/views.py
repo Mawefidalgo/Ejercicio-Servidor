2210edb67a1526c214f3f247c9a6dbd5f2482203
Mawefidalgo <mawefidalgo7@gmail.com>
2023-11-08 10:24:29 +0100

    Formulario actualizado

--- todolist/views.py
+++ todolist/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
+from .forms import TaskForm
 # Create your views here.
 def task_list(request):
     task = Task.objects.all()
-    return render(request, 'todolist/task_list.html', {"task":task})
+
+    if request.method == 'POST':
+        form=TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form= TaskForm()
+    return render(request, 'todolist/task_list.html', {"task":task, "form":form} )
+
