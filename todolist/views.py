576b7e0792cdbe1a13245ea5f1cefc178d61229f
Mawefidalgo <mawefidalgo7@gmail.com>
2023-11-10 14:12:43 +0100

    get y post

--- todolist/views.py
+++ todolist/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render, redirect
 from .models import Task
 from .forms import TaskForm
+from django.views import View
 # Create your views here.
-def task_list(request):
+
+class TaskList(View):
     task = Task.objects.all()
 
-    if request.method == 'POST':
+    def actuslizaTask(self):
+        self.task = Task.objects.all()
+        return self.task
+    
+    def get(self, request):
+        form= TaskForm()
+        return render(request, 'todolist/task_list.html', {"task":self.actuslizaTask(), "form":form} )
+    
+    def post(self, request):
         form=TaskForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('task_list')
-    else:
-        form= TaskForm()
-    return render(request, 'todolist/task_list.html', {"task":task, "form":form} )
+        return render(request, 'todolist/task_list.html', {"task":self.actuslizaTask(), "form":form} )
+    
 
